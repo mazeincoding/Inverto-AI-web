eeaabb3cc9f1914c430cf4ebd4474f068961979b
Maze Winther <innoworldgames@gmail.com>
2024-08-25 17:22:06 +0200

    Added redirect to home on logo click

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,9 +1,21 @@
+"use client";
+
 import Image from "next/image";
 import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 export function Logo({ is_responsive = false }: { is_responsive?: boolean }) {
+  const router = useRouter();
+
+  const handle_click = () => {
+    router.push("/");
+  };
+
   return (
-    <div className="flex items-center space-x-4">
+    <div
+      className="flex items-center space-x-4 cursor-pointer"
+      onClick={handle_click}
+    >
       <Image src="/logo.svg" alt="Logo" width={13} height={30} />
       <span
         className={cn(
