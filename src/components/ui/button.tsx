672e46612472aa1e7ba2b129fab007ed02efce36
Maze Winther <innoworldgames@gmail.com>
2024-08-28 19:18:18 +0200

    Changed styles of button and alert-dialog slightly

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-primary/15 bg-background hover:bg-primary/15 text-primary",
+          "border border-foreground/15 bg-background hover:bg-foreground/10 text-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-foreground",
